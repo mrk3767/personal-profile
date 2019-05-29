ebc144d4a37e3cb1facf273bcb9b21035fe6fe79
Michael R. Kenworthy <Mike@Kenworthy.io>
2019-05-28 21:41:46 -0700

    Clear the entire app to get a fresh start

--- src/pages/emergency.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
-
-export default () => (
-  <Typography>Emergency</Typography>
-)
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { withSiteData } from 'react-static'
-import Profile from '../components/Profile'
-
-export default withSiteData(() => (
-  <div style={{ textAlign: 'center' }}>
-    <Profile />
-  </div>
-))
